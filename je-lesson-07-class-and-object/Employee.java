38529eaa93d30d1478963124f202179ebb6a3d13
Aleksandra Ivanets <stenavisasha@mail.ru>
2025-05-20 21:55:30 +0300

    Lesson-7: Working with Class and object
    * created classes Employee and Car

---
+++ je-lesson-07-class-and-object/Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private String age;
+
+    public Employee(String name, String position, String email, String phone, String age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.age = age;
+    }
+}
